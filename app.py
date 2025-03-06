6ae8a93b99851fa23668baa30dbb3f079ce3557e
Lynn, Megan <Megan_Lynn@comcast.com>
2025-03-06 13:34:22 -0500

    Add a dynamic submission message notif after form completion

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -24,13 +24,14 @@ def index():
         last_name = request.form["last_name"]
         email = request.form["email"]
         date = request.form["date"]
-        date_obj = datetime.strptime(date, "%Y-%m,-%d")
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
         occupation = request.form["occupation"]
 
         form = Form(first_name=first_name, last_name=last_name,
                     email=email, date=date_obj, occupation=occupation)
         db.session.add(form)
         db.session.commit()
+        flash(f"{first_name}, Your form was submitted successfully!", "success")
 
     return render_template("index.html")
 
