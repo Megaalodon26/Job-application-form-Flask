d4622339e5ffe04e5c2dce493e66686a3d7362a3
Lynn, Megan <Megan_Lynn@comcast.com>
2025-03-06 13:25:20 -0500

    Add feature to show user data in the database

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+from datetime import datetime
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "..."
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
 db = SQLAlchemy(app)
 
 class Form(db.Model):
@@ -23,7 +24,13 @@ def index():
         last_name = request.form["last_name"]
         email = request.form["email"]
         date = request.form["date"]
+        date_obj = datetime.strptime(date, "%Y-%m,-%d")
         occupation = request.form["occupation"]
+
+        form = Form(first_name=first_name, last_name=last_name,
+                    email=email, date=date_obj, occupation=occupation)
+        db.session.add(form)
+        db.session.commit()
 
     return render_template("index.html")
 
